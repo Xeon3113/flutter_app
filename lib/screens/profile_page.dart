9d92637a4419aa0730001e00cab20b467053ad2d
Xeon <arangluv@gmail.com>
2019-12-13 00:10:20 +0900

    Xeon's first flutter practice app...

--- lib/screens/profile_page.dart
+++ lib/screens/profile_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_app/constants/size.dart';
 
 class ProfilePage extends StatefulWidget {
   @override
@@ -6,13 +7,87 @@ class ProfilePage extends StatefulWidget {
 }
 
 class _ProfilePageState extends State<ProfilePage> {
+  bool _menuOpened = false;
+  Size _size;
+  double _menuWidth;
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Center(
-        child: Text('profile page'),
+    _size = MediaQuery.of(context).size;
+    _menuWidth = _size.width / 1.5;
+
+    return Scaffold(
+      body: Stack(
+        children: <Widget>[
+          _sideMenu(),
+          _profile(),
+        ],
       ),
     );
   }
-}
 
+  Widget _sideMenu() {
+    return AnimatedContainer(
+      width: _menuWidth,
+      curve: Curves.easeInOut,
+      color: Colors.grey[200],
+      duration: Duration(milliseconds: 250),
+      transform: Matrix4.translationValues(
+          _menuOpened ? _size.width - _menuWidth : _size.width, 0, 0),
+      child: SafeArea(
+        child: Column(
+          mainAxisSize: MainAxisSize.max,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            FlatButton(child: Text('Xeon3113'), onPressed: null,)
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _profile() {
+    return AnimatedContainer(
+      curve: Curves.easeInOut,
+      duration: Duration(milliseconds: 250),
+      transform: Matrix4.translationValues(_menuOpened ? -_menuWidth : 0, 0, 0),
+      child: SafeArea(
+        child: Column(
+          children: <Widget>[
+            _userNameIconButton(),
+            Expanded(
+              child: CustomScrollView(
+                slivers: <Widget>[
+
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+
+  Row _userNameIconButton() {
+    return Row(
+      children: <Widget>[
+        Padding(
+          padding: const EdgeInsets.only(left: common_gap),
+          child: Text(
+            'Xeon3113',
+            style: TextStyle(
+                fontWeight: FontWeight.bold, fontSize: common_xxxl_fontSize),
+          ),
+        ),
+        Spacer(),
+        IconButton(
+            icon: Icon(Icons.menu),
+            onPressed: () {
+              setState(() {
+                _menuOpened = !_menuOpened;
+              });
+            }),
+      ],
+    );
+  }
+}
