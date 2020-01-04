231159c57c3d822bea2affc0ed6a31b348a70261
Xeon3113 <arangluv@gmail.com>
2020-01-05 00:22:52 +0900

    인트로 페이지 마무리 (Overflow 이슈 해결을 위해 ListView 대신 SingleChildScrollView 이용)

--- lib/widgets/sign_in_form.dart
+++ lib/widgets/sign_in_form.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 import 'package:flutter_app/constants/size.dart';
 import 'package:flutter_app/utils/simple_snackbar.dart';
 
@@ -24,117 +25,109 @@ class _SignInFormState extends State<SignInForm> {
   Widget build(BuildContext context) {
     return Scaffold(
       resizeToAvoidBottomInset: true,
-      body: Padding(
-        padding: const EdgeInsets.all(common_gap),
+      body: SingleChildScrollView(
+        padding: const EdgeInsets.only(top: 70, right: common_gap, bottom: common_gap, left: common_gap),
         child: Form(
           key: _formKey,
-          child: ListView(
-            shrinkWrap: true, //just set this property
-            /*
+          child: Column(
             mainAxisSize: MainAxisSize.max,
-            mainAxisAlignment: MainAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
             crossAxisAlignment: CrossAxisAlignment.stretch,
-            */
             children: <Widget>[
-              SizedBox(
-                height: 80,
-              ),
               Image.asset('assets/insta_text_logo.png'),
-              SizedBox(
-                height: 5,
-              ),
-              TextFormField(
-                controller: _emailController,
-                decoration: getTextFieldDecor("Email"),
-                validator: (String value) {
-                  if (value.isEmpty) {
-                    return 'Please enter your email address';
-                  } else if (!value.contains("@")) {
-                    return 'Please enter valid email address';
-                  }
-                  return null;
-                },
-              ),
-              SizedBox(
-                height: 5,
-              ),
-              TextFormField(
-                  controller: _pwController,
-                  decoration: getTextFieldDecor("Password"),
+              Padding(
+                padding: const EdgeInsets.only(top: common_s_gap),
+                child: TextFormField(
+                  controller: _emailController,
+                  decoration: getTextFieldDecor("Email"),
                   validator: (String value) {
-                    if (value.isEmpty) {
-                      return 'Please enter your password';
+                    if (value.isEmpty || !value.contains("@")) {
+                      return 'Please enter your email address';
                     }
                     return null;
-                  }),
-              SizedBox(
-                height: 10,
-              ),
-              Text(
-                'Forgotten password?',
-                textAlign: TextAlign.end,
-                style: TextStyle(
-                  color: Colors.blue[700],
-                  fontWeight: FontWeight.w600,
+                  },
                 ),
               ),
-              SizedBox(
-                height: 10,
+              Padding(
+                padding: const EdgeInsets.only(top: common_s_gap),
+                child: TextFormField(
+                    controller: _pwController,
+                    decoration: getTextFieldDecor("Password"),
+                    validator: (String value) {
+                      if (value.isEmpty) {
+                        return 'Please enter your password';
+                      }
+                      return null;
+                    }),
               ),
-              FlatButton(
-                onPressed: () {
-                  if (_formKey.currentState.validate()) {}
-                },
+              Padding(
+                padding: const EdgeInsets.only(top: common_s_gap),
                 child: Text(
-                  "Log in",
+                  'Forgotten password?',
+                  textAlign: TextAlign.end,
                   style: TextStyle(
-                    color: Colors.white,
+                    color: Colors.blue[700],
+                    fontWeight: FontWeight.w600,
                   ),
                 ),
-                color: Colors.blue,
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(6),
-                ),
               ),
-              SizedBox(
-                height: 10,
-              ),
-              Stack(
-                alignment: Alignment.center,
-                children: <Widget>[
-                  Positioned(
-                    left: 0,
-                    right: 0,
-                    height: 1,
-                    child: Container(
-                      color: Colors.grey[300],
-                      height: 1,
+              Padding(
+                padding: const EdgeInsets.only(top: common_s_gap),
+                child: FlatButton(
+                  onPressed: () {
+                    if (_formKey.currentState.validate()) {}
+                  },
+                  child: Text(
+                    "Log in",
+                    style: TextStyle(
+                      color: Colors.white,
                     ),
                   ),
-                  Container(
-                    height: 3,
-                    width: 50,
-                    color: Colors.grey[50],
+                  color: Colors.blue,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(6),
                   ),
-                  Text(
-                    'OR',
-                    style: TextStyle(
-                        color: Colors.grey, fontWeight: FontWeight.bold),
-                  ),
-                ],
-              ),
-              SizedBox(
-                height: 10,
-              ),
-              FlatButton.icon(
-                textColor: Colors.blue,
-                onPressed: () {
-                  simpleSnackBar(context, "facebook pressed");
-                },
-                icon: ImageIcon(
-                  AssetImage("assets/icon/facebook.png"),
                 ),
-                label: Text("Login with Facebook"),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(top: common_s_gap),
+                child: Stack(
+                  alignment: Alignment.center,
+                  children: <Widget>[
+                    Positioned(
+                      left: 0,
+                      right: 0,
+                      height: 1,
+                      child: Container(
+                        color: Colors.grey[300],
+                        height: 1,
+                      ),
+                    ),
+                    Container(
+                      height: 3,
+                      width: 50,
+                      color: Colors.grey[50],
+                    ),
+                    Text(
+                      'OR',
+                      style: TextStyle(
+                          color: Colors.grey, fontWeight: FontWeight.bold),
+                    ),
+                  ],
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(top: common_s_gap),
+                child: FlatButton.icon(
+                  textColor: Colors.blue,
+                  onPressed: () {
+                    simpleSnackBar(context, "facebook pressed");
+                  },
+                  icon: ImageIcon(
+                    AssetImage("assets/icon/facebook.png"),
+                  ),
+                  label: Text("Login with Facebook"),
+                ),
               ),
             ],
           ),
